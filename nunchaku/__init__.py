fac72a8c423f8ed514f21445f2f73740b6968027
Tom. M. <61305041+Tom-M-Git@users.noreply.github.com>
2026-07-29 02:17:57 +0900

    re-copied files from installed backend

--- nunchaku/__init__.py
+++ nunchaku/__init__.py
@@ -1,19 +1,19 @@
 from .models import (
-    NunchakuFlux2Transformer2DModel,
     NunchakuFluxTransformer2dModel,
     NunchakuFluxTransformer2DModelV2,
     NunchakuQwenImageTransformer2DModel,
     NunchakuSanaTransformer2DModel,
     NunchakuT5EncoderModel,
     NunchakuZImageTransformer2DModel,
-)
+
+    NunchakuFlux2Transformer2DModel,)
 
 __all__ = [
     "NunchakuFluxTransformer2dModel",
     "NunchakuSanaTransformer2DModel",
     "NunchakuT5EncoderModel",
     "NunchakuFluxTransformer2DModelV2",
-    "NunchakuFlux2Transformer2DModel",
     "NunchakuQwenImageTransformer2DModel",
     "NunchakuZImageTransformer2DModel",
-]
+
+    "NunchakuFlux2Transformer2DModel",]
